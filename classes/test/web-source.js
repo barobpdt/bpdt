3ec1d5bbcee6be524e2b5f683648864c74041630
DESKTOP-QVJ9AF3\user <isitna@gmail.com>
2025-06-19 07:13:00 +0900

    ws

--- classes/test/web-source.js
+++ classes/test/web-source.js
@@ -1,15 +1,25 @@
 ~~
+include('classes/common/json')
 @ws.addUrl('/pages/app', @ws.page)
-
-
-
+ 
 ~~
 <func>
 	pushArray(a,b) {
 		if(a.size()) a.insert(0,b) else a.add(b)
 		return b;
 	}
-	sfunc() {
+	log(&s) {
+		not(s.ch()) return;
+		line=s.findPos("\n")
+		while(a, args(), n) {
+			if(n) {
+				aa="$a"
+				line.add("\n\t[$n] ", aa.findPos("\n"))
+			}
+		}
+		print(line)
+	}
+	sfc() {
 		ss=''
 		while(a,args(),n) {
 			if(n.eq(0)) {
@@ -21,30 +31,7 @@
 		}
 		ss.add(')')
 		return ss;
-	}
-	ssfunc() {
-		ss=''
-		while(a,args(),n) {
-			if(n.eq(0)) {
-				ss.add(a,'(')
-				continue;
-			} 
-			if(n.gt(1)) ss.add(',')
-			if(typeof(a,'string')) {
-				if(typeof(a,'num')) {
-					ss.add(a)
-				} else if(a.eq('true','false','null','undefined')) {
-					ss.add(a)
-				} else {
-					ss.add(Cf.jsValue(a,true))
-				}
-			} else {
-				ss.add(a)
-			}
-		}
-		ss.add(')')
-		return ss;
-	}
+	} 
 	@ws.pagePath(&s) {
 		home = conf('web.rootPath')
 		a=s.findLast('/')
@@ -56,14 +43,7 @@
 				name = b
 			}
 		}
-	}
-	
-	@ws.page(req, param) {
-		srcFile = @ws.pagePath(req.getValue('url'))
-		src=fileRead(srcPath)
-		req.send(@ws.parseTemplate(src, param)) 
 	} 
-	
 	@ws.addUrl(url, fc) {
 		prev = map.get(url)
 		if(typeof(prev,'function')) {
@@ -76,7 +56,7 @@
 	
 	@ws.pagePath(&s) {
 		home = conf('web.rootPath')
-		print("s==>$s")
+		log("s==>$s")
 		if( s.find('/index.html') ) {
 			a=s.findPos('/index.html')
 			return Cf.val(home, a,".htm")
@@ -85,7 +65,7 @@
 	}
 	@ws.page(req, param) {
 		srcPath = @ws.pagePath(req.getValue('url'))
-		print("web page path => $srcPath")
+		log("web page path => $srcPath")
 		src=fileRead(srcPath)
 		param.set('@srcPath', srcPath)
 		req.send(@ws.parseTemplate(src, param)) 
@@ -100,26 +80,24 @@
 		}
 		not(fc) fc=@ws.page
 		map.set(url, fc)
-		print("map -> $map")
+		log("map -> $map")
 	}
-
-</func>
-
-~~
-<func>
-	@ws.getFnm(param) {
-		
+	 
+	@ws.setIncVars(cur, code, param) {
+		fidx = param.incrNum("@funcIndex")
+		cur.set('@incCode', code)
+		cur.set('@incFuncName',"${code}_$fidx")
+		cur.set('@incFuncSrc','')
+		cur.set('@incFuncScript','')
 	}
 	@ws.parseTemplate(&s, param) {
 		stack=param.addArray('@incStack')
 		map = param.addNode('@incMap')
 		cur = map.addNode('index')
+		@ws.setIncVars(cur,"index",param)
 		pushArray(stack,cur)
 		param.set('@funcNode', Cf.funcNode())
-		cur.copyNode(param)
-		cur.set('incFuncName',@ws.getFnm(param))
-		cur.set('incFuncSrc','')
-		cur.set('incFuncScript','')
+		
 		ss='', sp=0
 		while(s.valid()) {
 			c=s.ch()
@@ -136,13 +114,13 @@
 			ep=s.cur()
 			ss.add(s.value(sp,ep))
 			Cf.error(true)
-			sp = @ws.makePageVar(s, param)
+			sp = @ws.makePageVar(s, param) not(typeof(sp,'number')) return;
 			if(Cf.error()) {
 				ss.add(Cf.error() )
 				break	
 			}
 			if( sp<ep) {
-				print("parse template end point error ($sp, $ep) ")
+				log("parse template end point error ($sp, $ep) ")
 				break;
 			}
 			s.pos(sp)
@@ -166,12 +144,13 @@
 		c=s.ch()
 		fc = call("ws.$name")
 	
-		print("xxxxxxxxxx make page var xxxxxxxxx", name, fc, p)
+		log("xxxxxxxxxx make page var xxxxxxxxx", name, fc, p)
 		if( typeof(fc,'func')) {
 			ep = fc(s, p,param)
 		} else {
 			ep = s.cur()
 		}
+		log("xxxxxxxxxx $name xxxxxxxxx ep==$ep")
 		return ep;
 	}
 	@ws.vars(s,p,param) {
@@ -183,11 +162,11 @@
 			val = @json.nodeStr(cur)
 			funcNode.append('ss', "addVars($val)\n")
 		}
-		print("@@ vars => ", vars)
+		log("@@ vars => ", val)
 		return s.cur()
 	}
 	@ws.switch(s,p,param) {
-		print("switch == $p start")
+		log("switch == $p start")
 		while(s.valid()) {
 			not(@ws.isPageVar(s)) break;
 			sp = s.cur()
@@ -195,6 +174,7 @@
 			if( name.eq('case','default')) {
 				pp=s.match()
 				ep=@ws.case(s, pp,param, name, p)
+				log("switch case ep == $ep")
 				not(ep) return 0;
 				s.pos(ep)
 			} else {
@@ -206,10 +186,13 @@
 		return s.cur();
 	}
 	@ws.inc(s,p,param) {
-		print("ws inc[$p] ")
+		log("ws inc[$p] ")
 		return s.cur()
 	}
-	
+	@ws.case(s,p,param,type,pp) {
+		log("call case => ", p, param, type, pp)
+		return @ws.sub(s,param)
+	}	
 	@ws.isSingleTag(tag, &s) {
 		if(tag.eq('link','img','br') ) return true;
 		a=s.findPos('>')
@@ -223,7 +206,7 @@
 			ep = @ws.makePageVar(s,param)
 		} else {
 			c=s.ch()
-			print("make sub c==$c")
+			log("make sub c==$c")
 			if( c.eq() ) {
 				s.match()
 			} else if( c.eq('<')) {
@@ -232,6 +215,7 @@
 				if(c.eq('-')) s.incr().next()
 				ep=s.cur()
 				tag = s.trim(sp+1,ep)
+				log("tag == $tag")
 				if(@ws.isSingleTag(tag,s)) {
 					s.findPos('>',1)
 				} else {
@@ -246,9 +230,7 @@
 		}
 		return ep;
 	}
-</func>
-~~
-<func>
+ 
 	@ws.paramVal(param) {
 		c=s.ch(), v=null
 		if(c.eq()) {
@@ -268,7 +250,7 @@
 		param.inject(@vars, @incStack, @incMap )
 		root = incStack.get(0) not(root) root=param
 		name = s.move()
-		print("@@ var val ==> $name", name, vars)
+		log("@@ var val ==> $name", name, vars)
 		c=s.ch()
 		if(c) {
 			if(name.eq('global')) {
@@ -288,7 +270,7 @@
 	}
 	
 	@ws.checkOper(a,oper,b) {
-		print("xxxxxxxxx check oper xxxxxxxx",a,oper,b)
+		log("xxxxxxxxx check oper xxxxxxxx",a,oper,b)
 		switch(oper) {
 		case 1: return a.ne(b);
 		case 2: return a.eq(b);
@@ -329,4 +311,3 @@
 	}
 	
 </func>
-
